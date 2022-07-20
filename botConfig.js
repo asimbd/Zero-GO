4d3e370094d010a6c858d88586e1765648fa422c
Ubuntu <ubuntu@ip-172-31-41-18.ap-south-1.compute.internal>
2022-07-20 16:49:43 +0000

    first

---
+++ botConfig.js
@@ -0,0 +1,8 @@
+module.exports = {
+    token: "ODYwMTQ4NzY2NTkyOTkxMjQx.GrNDoT.-RRSsRf09TkdqV6m2pkSNK7e_Cg8npuVweQ4Js",
+    prefix: "m!",
+    admins: ["698115835163705344"
+],
+    debug: false,
+    countChannel: "999289744057962636"
+};
